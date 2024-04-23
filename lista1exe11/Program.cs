eec084de78338ec4641abff1479030cb59eaf735
User <User@DESKTOP-G0A3IIV>
2024-04-23 05:33:48 -0300

    Adicionar arquivos de projeto.

---
+++ lista1exe11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exe11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------");
+
+            Console.Write("Digite o valor de x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor de y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = Math.Pow(x, y);
+
+            Console.WriteLine($"{x} elevado a {y} é igual a {resultado}");
+        }
+    }
+}
